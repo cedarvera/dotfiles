4dfa3331dff8f27a57906f269e2a2189bd571c9f
Jon Yu <cedarvera@gmail.com>
2017-01-12 19:00:54 -0500

    Change to Noto font and use FontAwesome for conky.  Use dzen for showing the current and available workspaces.

--- home/xmonad/xmonad.hs
+++ home/xmonad/xmonad.hs
@@ -33,7 +33,52 @@ myTerminal :: String
 myTerminal = "terminology"
 
 myWorkspaces :: [String]
-myWorkspaces = [ "1", "2", "3", "4", "5" ]
+myWorkspaces = ["一" , "二" , "三" , "四" , "五" , "六" , "七" , "八" , "九" , "十"]
+--, "\xf0d1"
+
+-- colors
+-- black dark/light (color0 and color8)
+color00 = "#627484"
+color08 = "#6986A0"
+-- red dark/light (color1 and color9)
+color01 = "#E49F9F"
+color09 = "#E5BFBF"
+-- green dark/light (color2 and color10)
+color02 = "#91E380"
+color10 = "#AFE0A1"
+-- yellow dark/light (color3 and color11)
+color03 = "#EAE47C"
+color11 = "#F2FB9E"
+-- blue dark/light (color4 and color12)
+color12 = "#95ADD1"
+-- magenta dark/light (color5 and color13)
+color05 = "#DF9494"
+color13 = "#F2B0B0"
+-- cyan dark/light (color6 and color14)
+color06 = "#8CDBD8"
+color14 = "#B4F0F0"
+-- white dark/light (color7 and color15)
+color07 = "#E5E7EA"
+color15 = "#8CDBD8"
+
+-- Spit empty string
+outEmpty :: String -> String
+outEmpty str = ""
+
+logbar h = do
+  dynamicLogWithPP $ tryPP h
+tryPP :: Handle -> PP
+tryPP h = defaultPP
+        { ppOutput          = hPutStrLn h
+        , ppCurrent         = dzenColor (color08) (color14) . pad
+        , ppVisible         = dzenColor (color07) (color08) . pad
+        , ppHidden          = dzenColor (color07) (color08) . pad
+        , ppHiddenNoWindows = dzenColor (color07) (color08) . pad
+        , ppUrgent          = dzenColor (color07) (color13) . pad
+        , ppOrder           = \(ws:l:t) -> [ws,l]
+        , ppSep             = ""
+        , ppLayout          = outEmpty
+        }
 
 myKeys conf@(XConfig { XMonad.modMask = modMask }) = M.fromList $
   [ ((modMask, xK_w), swapNextScreen)
@@ -74,6 +119,8 @@ myManageHook = composeAll
   ]
 
 main = do
+  leftBar <- spawnPipe "dzen2 -ta l -p -w 276 -y 4 -x 8 -h 24 -fn 'Noto Sans CJK TC' -e ''"
+  -- leftBarN <- spawnPipe "dzen2 -ta l -p -w 300 -y 10 -x 278 -h 24 -fn 'FontAwesome' -e ''"
   xmonad $ ewmh defaultConfig
     { terminal           = myTerminal
     , workspaces         = myWorkspaces
@@ -81,9 +128,10 @@ main = do
     , modMask            = mod4Mask
     , focusFollowsMouse  = False
     , borderWidth        = 2
-    , normalBorderColor  = "#0A0A0A"
-    , focusedBorderColor = "#112233"
+    , normalBorderColor  = color08
+    , focusedBorderColor = color01
     , layoutHook         = windowArrange myLayoutHook
     , handleEventHook    = myEventHook
     , manageHook         = manageDocks <+> myManageHook
+    , logHook            = logbar leftBar -- <+> leftBarN
     }
